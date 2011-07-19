cddb3285ab609058513f71b9c308ac4a927bd1e8
chompy <me@chompy.co>
2011-07-19 12:36:33 -0400

    - Delete unused files, renamed some stuff.

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 from cx_Freeze import setup, Executable
 
+includes = ["pygame"]
+excludes = ['_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 'pywin.debugger',
+            'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
+            'Tkconstants', 'Tkinter']
+packages = []
+path = []           
+
+GUI2Exe_Target_1 = Executable(
+    # what to build
+    script = "main.py",
+    initScript = None,
+    base = 'Win32GUI',
+    targetDir = r"dist",
+    targetName = "upchomp.exe",
+    compress = True,
+    copyDependentFiles = True,
+    appendScriptToExe = False,
+    appendScriptToLibrary = False,
+    icon = "icon.ico"
+    )            
+            
 setup(
         name = "UpChomp",
         version = "1.0",
-        description = "Help Chompy get cookies!",
-        executables = [Executable("main.py")]
+        description = "Help Chompy get cookies!",   
+        options = {"build_exe": {"includes": includes,
+                                 "excludes": excludes,
+                                 "packages": packages,
+                                 "path": path
+                                 }
+                   },   
+        executables = [GUI2Exe_Target_1]                        
         )
